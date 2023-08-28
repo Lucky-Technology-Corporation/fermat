21a42631320191bd5f42976a6feb86ea89e8b0ab
Sean Walsh <sdwalsh@mirango.io>
2023-08-28 14:54:37 -0700

    script

---
+++ scripts/fermat-downloader.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export GOOGLE_APPLICATION_CREDENTIALS="/home/swizzle_prod_user/.config/gcloud/application_default_credentials.json"
+
+GCS_URL="https://storage.cloud.google.com/swizzle_scripts/fermat"
+TOKEN=$(gcloud auth print-access-token)
+
+curl -H "Authorization: Bearer $TOKEN" -o fermat-linux "$GCS_URL"
+
+if [ $? -eq 0 ]; then
+    echo "Download successful!"
+else
+    echo "Download failed!"
+fi
