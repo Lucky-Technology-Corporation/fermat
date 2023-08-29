76409429fa81a9273bb6d874ecd0639a2e960caa
Sean Walsh <sdwalsh@mirango.io>
2023-08-29 10:43:52 -0700

    clean and fix

--- scripts/fermat-downloader.sh
+++ scripts/fermat-downloader.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
+# Set the path to your service account key file
 export GOOGLE_APPLICATION_CREDENTIALS="/home/swizzle_prod_user/.config/gcloud/application_default_credentials.json"
+echo "GOOGLE_APPLICATION_CREDENTIALS set to $GOOGLE_APPLICATION_CREDENTIALS"
 
-GCS_URL="https://storage.cloud.google.com/swizzle_scripts/fermat"
+# Authenticate and fetch an access token
+echo "Fetching access token..."
 TOKEN=$(gcloud auth print-access-token)
+if [[ -z "$TOKEN" ]]; then
+    echo "Failed to fetch access token!"
+    exit 1
+fi
+echo "Access token fetched successfully."
 
-if curl -H "Authorization: Bearer $TOKEN" -o fermat-linux "$GCS_URL"; then
+# Define GCS URL
+GCS_URL="https://storage.cloud.google.com/swizzle_scripts/fermat-linux"
+
+# Download the file and print the response
+echo "Attempting to download from $GCS_URL..."
+RESPONSE=$(curl -L -H "Authorization: Bearer $TOKEN" -v -o fermat-linux "$GCS_URL" -w '%{http_code}' -s)
+if [ "$RESPONSE" == "200" ]; then
     echo "Download successful!"
 else
-    echo "Download failed!"
+    echo "Download failed with HTTP status code: $RESPONSE"
+    exit 1
 fi
